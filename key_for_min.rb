4a696b2845568e6d7f258ab2d56e3edee75745c5
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-20 21:08:27 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash.empty?
+    return nil
+  end
 
+  name_hash.each_pair do |k, v|
+     name_hash.each_pair do |k1, v1|
+      name_hash.each_pair do |k2, v2|
+        if v < v1 && v1 < v2
+          return k
+        end
+      end
+     end    
+  end
+  
 end
