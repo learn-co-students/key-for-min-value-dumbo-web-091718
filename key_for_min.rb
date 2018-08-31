f01caf9a731ca3e67ecc4fd6b6f25b6eab4af50e
Nasser Sanou <nassersanou23@gmail.com>
2018-08-31 00:59:55 +0000

    in progress

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,26 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+# # prereqs: iterators, hashes, conditional logic
+# # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash.empty?
+    return nil 
+  end 
+  i = 0
+  while i < (name_hash.length) 
+  lowest_key = nil
+  name_hash.each do |key, value|
+    min_value = 0
+    if value[i-1] < value
+      lowest_key = key 
+      # return key
+    # if value < min_value
+    #       min_value = value
+    #       min_key = key
+    return key
+    end 
+  end 
+end 
 
 end
+
+=
