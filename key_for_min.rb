0560e6a7b28299a37c3d40429f1dddc08e98fb0e
Nasser Sanou <nassersanou23@gmail.com>
2018-08-31 03:04:32 +0000

    refactored min_val

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,26 +1,35 @@
 # # prereqs: iterators, hashes, conditional logic
 # # Given a hash with numeric values, return the key for the smallest value
 
+# def key_for_min_value(name_hash)
+#   if name_hash.empty?
+#     return nil 
+#   end 
+#   i = 0
+#   while i < (name_hash.length) 
+#   lowest_key = nil
+#   name_hash.each do |key, value|
+#     min_value = 0
+#     if value[i-1] < value
+#       lowest_key = key 
+#       # return key
+#     # if value < min_value
+#     #       min_value = value
+#     #       min_key = key
+#     return key
+#     end 
+#   end 
+# end 
+# end
+#BETTER & SIMPLE logic: mk = minimum Key.mv = minimum value
 def key_for_min_value(name_hash)
-  if name_hash.empty?
-    return nil 
-  end 
-  i = 0
-  while i < (name_hash.length) 
-  lowest_key = nil
-  name_hash.each do |key, value|
-    min_value = 0
-    if value[i-1] < value
-      lowest_key = key 
-      # return key
-    # if value < min_value
-    #       min_value = value
-    #       min_key = key
-    return key
-    end 
-  end 
-end 
-
+  mv = nil && mk = nil
+  name_hash.each do |k, v|
+    if mv == nil
+     (mv = v) && (mk = k) end 
+    if mv > v 
+    (mv = v) && (mk = k) end
+  end
+  mk
 end
 
-=
